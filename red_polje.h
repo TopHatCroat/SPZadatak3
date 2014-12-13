9f5079d4d5b6b8a5d9bafafda852ee64c7ab9cfd
TopHatCroat <martinovic.antonio@gmail.com>
2014-12-13 18:22:17 +0100

    Create red_polje.h

---
+++ red_polje.h
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+struct element {
+	char ime[20];
+	char prezime[20];
+	char spol;
+	int datum[3];
+	int cetvorka[4];
+};
+
+struct red {
+	element popis[10000];
+	int front;
+	int rear;
+};
+
+int AddOne(int n){
+	return ((n+1) % 10000);
+}
+
+bool IsEmptyQ(red *Q){
+	if(AddOne(Q->rear) == Q->front) return true;
+	return false;
+}
+
+
+
+element FrontQ(red *Q){
+	if (IsEmptyQ(Q)){
+		cout << "(FrontQ) - Red je prazan" << endl;
+	}
+	else return Q->popis[Q->front];
+}
+
+void EnQueueQ(element x, red *Q){
+	Q->rear = AddOne(Q->rear);
+	Q->popis[Q->rear] = x;
+}
+
+void DeQueueQ(red *Q){
+	if(IsEmptyQ(Q)) return;
+//	if(Q->front == 9999) Q->front = 0;
+//	Q->front++;
+	Q->front = AddOne(Q->front);
+}
+
+red *InitQ(){
+	red *n = new red;
+	n->front=0;
+	n->rear=9999;
+	return n;
+}
+
